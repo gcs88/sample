5948aeded6edd82e6eb9090c17b5ba01ad889950
VELATIA\gorka.calle <gorka.calle@ikusi.com>
2017-12-27 15:26:35 +0100

    edit file to test Jenkins

--- sample.c
+++ sample.c
@@ -2,6 +2,6 @@
 
 int main()  
 {  
-    printf("Hello, World! This is a native C program compiled on the command line.\n");  
+    printf("Hello, World! This is a native C program compiled on the command line!!\n");  
     return 0;  
 }   
