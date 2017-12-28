76b9440a1804f7dc55cfb5d08f41f120301d66d5
VELATIA\gorka.calle <gorka.calle@ikusi.com>
2017-12-28 12:55:08 +0100

    deliberated failure

--- sample.c
+++ sample.c
@@ -2,7 +2,7 @@
 
 int main()  
 {  
-    printf("Hello, World! This is a native C program compiled on the command line\n")
+    printf("Hello, World! This is a native C program compiled on the command line\n"
     return 0;  
 	
 }   
