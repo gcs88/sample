39936635ba6436b80cee9302b2821f7c565e4ee8
VELATIA\gorka.calle <gorka.calle@ikusi.com>
2018-10-26 11:53:26 +0200

    new change

--- sample.c
+++ sample.c
@@ -1,8 +1,8 @@
-#include <stdio.h>  
+#include <stdio.h>
 
-int main()  
-{  
-    printf("Hello, World! This is a native C program compiled on the command line\n");
-    return 0;  
-	
-}   
+int main()
+{
+    printf("Hello, World! This is a native C program compiled on the command line\n")
+    return 0;
+
+}
